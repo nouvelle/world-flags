30f73db90ee522ea614abfae7b856f666ebaac98
nouvelle <nouvellenuts@gmail.com>
2020-10-05 21:48:03 +0900

    Added the functionality to move to the detailed page of the border countries when click on the country name

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,13 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { RootContext } from '../../App';
 
 import './Detail.css';
 
 function Detail() {
-  const [, , detail, , , setIsAll] = useContext(RootContext);
-  console.log(detail)
+  const [borders, setBorders] = useState("");
+  const [, , detail, setDetail, , setIsAll] = useContext(RootContext);
+  let countryInfo = {};
 
-  const countryInfo = {
+  // border Countries のデータ取得
+  useEffect(() => {
+    let borderCountries = []
+    getBorderCountryData();
+
+    async function getBorderCountryData() {
+      if(detail[0].borders.length > 0){
+        console.log(detail[0].borders)
+  
+        for(let i = 0; i < detail[0].borders.length; i++) {
+          const url = `https://restcountries.eu/rest/v2/alpha/${detail[0].borders[i]}`;
+          await fetch(url)
+          .then(res => res.json())
+          .then(data => borderCountries.push(data))
+          .catch(err => console.log("Err :", err))
+        }
+        console.log(borderCountries)
+        setBorders(borderCountries)
+    }
+    }
+  }, [detail]);
+
+  countryInfo = {
     nativeName: detail[0].nativeName ? detail[0].nativeName : "",
     population: detail[0].population ? detail[0].population.toLocaleString() : "",
     region: detail[0].region ? detail[0].region : "",
@@ -16,8 +39,18 @@ function Detail() {
     topLevelDomain: detail[0].topLevelDomain ? detail[0].topLevelDomain.map(dmain => dmain + ", ") : "",
     currencies: detail[0].currencies ? detail[0].currencies.map(obj => obj.name + ", ") : "",
     languages: detail[0].languages ? detail[0].languages.map(obj => obj.name + ", ") : "",
-    borders: detail[0].borders ? detail[0].borders.map(name => name + ", ") : "",
   }
+
+  function clickBorderCountry(name) {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        setDetail(data);
+      })
+      .catch(err => console.log("Err :", err))
+  }
+
   function changeView(){
     setIsAll(true);
   }
@@ -39,7 +72,20 @@ function Detail() {
           <div><span className="title">Top Level Domain: </span>{countryInfo.topLevelDomain}</div>
           <div><span className="title">Currencies: </span>{countryInfo.currencies}</div>
           <div><span className="title">Languages: </span>{countryInfo.languages}</div>
-          <div><span className="title">Border Countries: </span>{countryInfo.borders}</div>
+          <div><span className="title">Border Countries: </span>
+            {(() => {
+              if(borders) {
+                return borders.map((country, i) => {
+                  return (
+                    <div 
+                      key={i}
+                      onClick={() => clickBorderCountry(country.name)}
+                    >{country.name}</div>
+                  )
+                })
+              }
+            })()}
+          </div>
         </div>
       </div>
     </div>
