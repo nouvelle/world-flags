c84ee0c4c004d4a9b847b4cce5d5ad9af39b83a1
nouvelle <nouvellenuts@gmail.com>
2020-10-08 17:38:51 +0900

    remove some comment

--- src/components/All/Filter.jsx
+++ src/components/All/Filter.jsx
@@ -53,17 +53,3 @@ function Filter() {
 }
 
 export default Filter;
-
-{/* <select
-name="region"
-onChange={handleChange}
-defaultValue={'DEFAULT'}
->
-<option value="DEFAULT" disabled>Filter by Region</option>
-<option value="All">All</option>
-<option value="Africa">Africa</option>
-<option value="Americas">Americas</option>
-<option value="Asia">Asia</option>
-<option value="Europe">Europe</option>
-<option value="Oceania">Oceania</option>
-</select> */}
